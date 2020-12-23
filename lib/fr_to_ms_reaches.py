0f59d54ea91146e72701d2e90e3c0ab5e2588085
Ryan Spies <1589910547@mil>
2020-12-23 16:46:15 -0600

    using shapely for subsetting

--- lib/fr_to_ms_reaches.py
+++ lib/fr_to_ms_reaches.py
@@ -9,6 +9,7 @@ Description:
 
 import sys
 import geopandas as gpd
+from shapely.geometry import Point, LineString, MultiPoint
 
 fr_split_flows_fileName   = sys.argv[1]
 ms_flows_fileName         = sys.argv[2]
@@ -20,6 +21,10 @@ ms_flows = gpd.read_file(ms_flows_fileName)
 
 ms_flows_buffer = ms_flows.copy()
 ms_flows_buffer['geometry'] = ms_flows.buffer(10) # 10 meter buffer around lines
-ms_split_flows_gdf = gpd.sjoin(fr_split_flows, ms_flows_buffer, how='left', op='within')
+ms_flows_buffer['union'] = 'union' # add dummy column to dissolve all geometries into one
+ms_flows_buffer_search = ms_flows_buffer.dissolve(by='union').geometry[0]  # take the single union geometry
+#ms_split_flows_subset = gpd.sjoin(fr_split_flows, ms_flows_buffer, how='left', op='within')
 
-ms_split_flows_gdf.to_file(ms_split_flows_fineName,driver='GPKG',index=False)
+ms_split_flows_subset = fr_split_flows[fr_split_flows.within(ms_flows_buffer_search)]
+
+ms_split_flows_subset.to_file(ms_split_flows_fineName,driver='GPKG',index=False)
